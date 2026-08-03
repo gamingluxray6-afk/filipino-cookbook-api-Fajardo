6e397d0a388ea2ae9d5676291e2136ac6f09ee3b
kyleeee <gaming.luxray.6@gmail.com>
2026-08-03 11:02:32 +0800

    Add files via upload

---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+const API_TOKEN = 'YOUR_API_TOKEN';
+const DB_HOST = 'localhost';
+const DB_NAME = 'filipino_cookbook_api';
+const DB_USER = 'YOUR_DATABASE_USERNAME';
+const DB_PASS = 'YOUR_DATABASE_PASSWORD';
